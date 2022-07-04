69d39b657382a6ee3d3b8591c3e6b4c645c62934
JMiguelPerez <105886366+JMiguelPerez@users.noreply.github.com>
2022-07-04 12:51:14 -0400

    local server is running and connected to Database

--- GainBoyApplication/backend/api/workout.route.js
+++ GainBoyApplication/backend/api/workout.route.js
@@ -1,6 +1,8 @@
 import express from "express"
+
 const router = express.Router()
 
-router.route("/").get((req,res)=> res.send("first route"))
+router.route("/").get((req,res)=>res.send("hello world"))
+
 
 export default router
--- GainBoyApplication/backend/index.js
+++ GainBoyApplication/backend/index.js
@@ -1,30 +1,25 @@
-//Connect Database and Server
-import app from "./server.js"
+import app from "./server.js";
 import mongodb from "mongodb"
 import dotenv from "dotenv"
-
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 
 const port = process.env.PORT || 8000
 
-//acessing data
 MongoClient.connect(
-    process.env.GAINBOY_DB_URI,
-    { //settings
-        poolSize: 50, //50 users at a time
-        wtimeout: 2500, //2500 ms request times out
-        useNewUrlParse: true
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        maxPoolSize:50,
+        wtimeoutMS:2500,
+        useNewUrlParser:true
+    }
+)
+.catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+})
+.then(async client => {
+    app.listen(port, () => {
+        console.log('listening on port '+port)
     })
-
-    .catch(err => {
-        console.error(err.stack)
-        process.exit(1)
-    })
-
-    .then(async client  => 
-        {
-        app.listen(port, () => {
-            console.log(`listening on port ${port}`)
-        })
-    })
+})
