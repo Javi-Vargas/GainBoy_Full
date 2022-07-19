c232f313c74a73b22c5157dd781e77d87fca6a32
Javi-Vargas <105886463+Javi-Vargas@users.noreply.github.com>
2022-07-19 15:23:54 -0400

    Add files via upload

---
+++ MobileGainBoy/MobileGainBoy/src/AddWorkoutScreen.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { View, Text, Button, StyleSheet, ScrollView, SafeAreaView, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Feather from 'react-native-vector-icons/Feather'
+import LoginScreen from "./LoginScreen";
+
+const WorkoutScreen = ({ navigation }) => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={{ padding: 20, }}>
+                <View style={{ padding: 20 }}>
+                    <Text>Workout Info</Text>
+                </View>
+                <View style={{ flexDirection: 'column', borderColor: 'black', borderWidth: 1, borderRadius: 8 }}>
+                    <TextInput style={styles.nameBox} placeholder="Workout Name:" />
+                    <TextInput style={styles.descriptionBox} placeholder="Description:" />
+                </View>
+                <View style={{ paddingTop: 20 }}>
+                    <Text>Exercises: </Text>
+                    <TouchableOpacity style={styles.addExercise}>
+                        <Ionicons name="add-circle-outline" color="blue" size="30%" />
+                        <Text style={{ paddingTop: 8 }}>Add Exercises</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+            <View style={{ flexDirection: 'row', justifyContent: 'center', justifyContent: 'space-evenly' }}>
+                <Button style={styles.saveBtn}
+                    title="Save"
+                    onPress={() => alert("Add Workout To Database")}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default WorkoutScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#8fcbbc",
+    },
+    addExercise: {
+        flexDirection: 'row',
+        paddingTop: 20,
+    },
+    descriptionBox: {
+        paddingTop: 10,
+        paddingBottom: 50,
+        borderTopColor: 'black'
+    },
+    nameBox: {
+        paddingTop: 0,
+    },
+    editBtn: {
+        left: 10,
+        top: 10,
+    },
+    plusBtn: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+    },
+    saveBtn: {
+        fontSize: 50
+    }
+})
---
+++ MobileGainBoy/MobileGainBoy/src/WorkoutScreen.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { View, Text, Button, StyleSheet, ScrollView, SafeAreaView, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Feather from 'react-native-vector-icons/Feather'
+import AddWorkoutScreen from './AddWorkoutScreen';
+
+const WorkoutScreen = ({ navigation }) => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={{ padding: 20, }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <Button title="Edit" onPress={() => alert("Edit Workout")} />
+                    <TouchableOpacity onPress={() => navigation.navigate('AddWorkout')}>
+                        <Ionicons name="add-outline" color="blue" size="30%" />
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flexDirection: 'row', borderColor: 'black', borderWidth: 1, borderRadius: 8, paddingHorizontal: 10, paddingVertical: 8 }}>
+                    <Feather name='search' size={20} color="black" style={{ fontSize: 30, marginRight: 5 }} />
+                    <TextInput placeholder="Search Saved Workouts" />
+                </View>
+
+                <View style={{ top: 30, flexDirection: 'row', justifyContent: 'center', paddingTop: 20, paddingBottom: 20, backgroundColor: "orange", borderRadius: 10 }}>
+                    <TouchableOpacity style={styles.addWorkout}>
+                        <Text>Start A Workout</Text>
+                    </TouchableOpacity>
+                </View>
+                <View >
+                    <Text style={{ paddingTop: 60, borderRadius: 20 }}>I wanna make this a list with the users saved Workouts</Text>
+                </View>
+            </ScrollView>
+            <Button
+                title="LogOut"
+                onPress={() => navigation.navigate('Login')}
+            />
+        </SafeAreaView >
+    )
+}
+
+export default WorkoutScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#8fcbbc",
+    },
+    addWorkout: {
+        flexDirection: 'row',
+    },
+    editBtn: {
+        left: 10,
+        top: 10,
+    },
+    plusBtn: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+    },
+    WO_list: {
+        borderColor: 'black',
+
+    }
+})
