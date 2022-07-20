be652f5de92126aac1de21522141b545c6d7bc9d
Javi-Vargas <105886463+Javi-Vargas@users.noreply.github.com>
2022-07-20 13:22:21 -0400

    Add files via upload
    These provide the logic for having a bottomTabMenu, and a drawer that can display the User's profile info.

---
+++ MobileGainBoy/MobileGainBoy/navigation/tabs.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Feather from 'react-native-vector-icons/Feather'
+
+import ProfileScreen from "../screens/ProfileScreen";
+import WorkoutScreen from "../screens/WorkoutScreen";
+import LogScreen from "../screens/LogScreen";
+
+const Tab = createBottomTabNavigator();
+const Tabs = () => {
+    return (
+        <Tab.Navigator screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: true,
+            tabBarStyle: { backgroundColor: 'grey' },
+            tabBarInactiveTintColor: "white",
+            tabBarActiveTintColor: "yellow",
+
+        }}>
+            <Tab.Screen name="Workout" component={WorkoutScreen} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="barbell-outline" color={color} size={size} />
+                )
+            }} />
+            <Tab.Screen name="Logs" component={LogScreen} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="bar-chart-outline" color={color} size={size} />
+                )
+            }} />
+            <Tab.Screen name="Profile" component={ProfileScreen} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="body-outline" color={color} size={size} />
+                )
+            }} />
+        </Tab.Navigator>
+    );
+
+}
+
+export default Tabs;
