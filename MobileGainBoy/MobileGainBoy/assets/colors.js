8e7bfd060ba31c71016012113684e10585f503ad
Javi-Vargas <105886463+Javi-Vargas@users.noreply.github.com>
2022-07-27 17:50:28 -0400

    A colors file to be imported on pretty much every page.
    Call these colors by using 'colors.red, colors.blue...etc"

---
+++ MobileGainBoy/MobileGainBoy/assets/colors.js
@@ -0,0 +1,11 @@
+export default {
+    red: '#E2202C',
+    blue: '#1EAAF1',
+    black: "#3C3A3A",
+    blackLite: "#595757",
+    white: "#EDEDDD",
+    purple: "#D024FF",
+    CJpurple: '#A482FF',
+    green: "#8DF77D",
+
+}
