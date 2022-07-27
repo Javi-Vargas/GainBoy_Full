0c5b6d079ffc0a8e155bca5ed081a5d06e8be88d
Javi-Vargas <105886463+Javi-Vargas@users.noreply.github.com>
2022-07-27 18:17:41 -0400

    Update BeginWorkoutScreen.js

--- MobileGainBoy/MobileGainBoy/src/screens/BeginWorkoutScreen.js
+++ MobileGainBoy/MobileGainBoy/src/screens/BeginWorkoutScreen.js
@@ -3,18 +3,24 @@ import { SafeAreaView, View, Text, Button, StyleSheet, StatusBar, TouchableOpaci
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import colors from "../assets/colors";
 
 const BeginWorkoutScreen = ({ navigation }) => {
     return (
         <GestureHandlerRootView style={styles.drawer}>
             <View>
                 <SafeAreaView style={{ paddingTop: 20, paddingHorizontal: 10 }}>
-                    <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: 47 }} onPress={() => navigation.openDrawer()}>
+                    {/* <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: 47 }} onPress={() => navigation.openDrawer()}>
                         <Ionicons name="menu-outline" color="blue" size={30} />
-                    </TouchableOpacity>
-                    <View style={{ top: 30, flexDirection: 'row', justifyContent: 'center', paddingTop: 20, paddingBottom: 20, backgroundColor: "orange", borderRadius: 10 }}>
-                        <TouchableOpacity style={{ flex: 'row', paddingHorizontal: 20 }}>
-                            <Text>Start An Empty Workout</Text>
+                    </TouchableOpacity> */}
+                    <View style={{
+                        top: 30, flexDirection: 'row', justifyContent: 'center', paddingTop: 20, paddingBottom: 20, backgroundColor: colors.CJpurple, borderRadius: 10, top: "30%", shadowColor: 'black',
+                        shadowRadius: 3,
+                        shadowOffset: { width: -5, height: 7, },
+                        shadowOpacity: 0.5,
+                    }}>
+                        <TouchableOpacity style={{ flex: 'row', paddingHorizontal: 20, borderRadius: 10 }}>
+                            <Text style={{ color: colors.white }}>Begin FreeStyle Workout</Text>
                         </TouchableOpacity>
                     </View>
                 </SafeAreaView>
@@ -36,7 +42,8 @@ const styles = StyleSheet.create({
     },
     drawer: {
         flex: 1,
-        backgroundColor: "#8fcbbc",
+        backgroundColor: colors.black,
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+        paddingHorizontal: 15
     }
 })
