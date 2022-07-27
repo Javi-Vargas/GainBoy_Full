6d1b6b2a708aea122a29407fdc53c85dc6854346
Javi-Vargas <105886463+Javi-Vargas@users.noreply.github.com>
2022-07-27 18:19:23 -0400

    Update LandingScreen.js

--- MobileGainBoy/MobileGainBoy/src/screens/LandingScreen.js
+++ MobileGainBoy/MobileGainBoy/src/screens/LandingScreen.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Tabs from '../../navigation/tabs';
+import Tabs from '../navigation/tabs';
 
 const LandingScreen = ({ navigation }) => {
     return (
@@ -11,12 +8,3 @@ const LandingScreen = ({ navigation }) => {
 }
 
 export default LandingScreen;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: "white",
-    }
-})
