4ed9a9404046cf46cddc39f15f5f4b7551cc506a
Javi-Vargas <105886463+Javi-Vargas@users.noreply.github.com>
2022-07-27 18:17:02 -0400

    Update AddWorkoutScreen.js
    Changed the look to match the Prototype

--- MobileGainBoy/MobileGainBoy/src/screens/AddWorkoutScreen.js
+++ MobileGainBoy/MobileGainBoy/src/screens/AddWorkoutScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, Button, StyleSheet, ScrollView, SafeAreaView, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import Feather from 'react-native-vector-icons/Feather'
+import colors from "../assets/colors";
 
 // PREPROCESSORS for Unit testing
 const UNIT_ADD_WORKOUT = false;
@@ -12,42 +13,28 @@ const AddWorkoutScreen = ({ navigation }) => {
 
         // <----------UNIT TESTING----------> 
         //  Automated test for adding a workout
-        if (UNIT_ADD_WORKOUT)
-        {
-            obj = {token: global.token, name: 'something', userId: '777', reps: '99', sets: '99', totalWeight: '9000', timeSpent: '99'};
+        if (UNIT_ADD_WORKOUT) {
+            obj = { name: 'something', userId: '777', reps: '99', sets: '99', totalWeight: '9000', timeSpent: '99' };
         }
         // --------------------------------->
-        else
-        {
+        else {
             //TODO: Fill me up correctly
-            //obj = {token: global.token, name: , userId: global.userId, reps: , sets: , totalWeight: , timeSpent: };
+            //obj = {name: , userId: global.userId, reps: , sets: , totalWeight: , timeSpent: };
         }
-        
+
         var js = JSON.stringify(obj);
 
         const response = await fetch(
             'https://gainzboy.herokuapp.com/auth/createWorkout',
-            {method:'POST', body:js, headers:{'Content-Type': 'application/json'}}
+            { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } }
         );
 
         var res = JSON.parse(await response.text());
-        
-        if(res.status != undefined)
-        {
+
+        if (res.status != undefined) {
             alert('Error occurred adding workout');
         }
-        else
-        {
-            // Cache the workout id
-            if (UNIT_ADD_WORKOUT)
-            {
-                global.exerciseMap.set('something', res._id);
-            }
-            else
-            {
-                //global.exerciseMap.set('insert variable storing name here', res._id);
-            }
-            
+        else {
             // Navigation is a property given from the Stack.Screen component in App.js. Inside this 'navigation' property 
             // is a function called navigate() that takes the name of another screen, in this case 'Landing', again defined in App.js
             navigation.navigate('Landing');
@@ -58,25 +45,24 @@ const AddWorkoutScreen = ({ navigation }) => {
         <SafeAreaView style={styles.container}>
             <ScrollView style={{ padding: 20, }}>
                 <View style={{ padding: 20 }}>
-                    <Text>Workout Info</Text>
+                    <Text style={{ color: colors.green, fontWeight: 'bold', fontSize: 25 }}>Workout Info</Text>
                 </View>
                 <View style={{ flexDirection: 'column', borderColor: 'black', borderWidth: 1, borderRadius: 8 }}>
                     <TextInput style={styles.nameBox} placeholder="Workout Name:" />
-                    <TextInput style={styles.descriptionBox} placeholder="Description:" />
+                    <TextInput style={styles.descriptionBox} placeholder="Description/Notes:" />
                 </View>
-                <View style={{ paddingTop: 20 }}>
-                    <Text>Exercises: </Text>
+                <View style={{ paddingTop: 45 }}>
+                    <Text style={{ color: colors.green, fontSize: 20 }}>Exercises: </Text>
                     <TouchableOpacity style={styles.addExercise}>
-                        <Ionicons name="add-circle-outline" color="blue" size={30} />
-                        <Text style={{ paddingTop: 8 }}>Add Exercises</Text>
+                        <Ionicons name="add-circle-outline" color={colors.green} size={30} />
+                        <Text style={{ paddingTop: 8, paddingLeft: 8, color: colors.green, fontWeight: 'bold' }}>Add Exercises</Text>
                     </TouchableOpacity>
                 </View>
             </ScrollView>
             <View style={{ flexDirection: 'row', justifyContent: 'center', justifyContent: 'space-evenly' }}>
-                <Button style={styles.saveBtn}
-                    title="Save"
-                    onPress={() => handleSave()}
-                />
+                <TouchableOpacity onPress={() => handleSave()}>
+                    <Text style={styles.saveBtn}>Save</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
@@ -87,7 +73,7 @@ export default AddWorkoutScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#8fcbbc",
+        backgroundColor: colors.black,
     },
     addExercise: {
         flexDirection: 'row',
@@ -95,11 +81,19 @@ const styles = StyleSheet.create({
     },
     descriptionBox: {
         paddingTop: 10,
+        paddingLeft: 10,
         paddingBottom: 50,
-        borderTopColor: 'black'
+        borderTopColor: 'black',
+        backgroundColor: colors.white,
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10
     },
     nameBox: {
-        paddingTop: 0,
+        paddingTop: 10,
+        paddingLeft: 10,
+        backgroundColor: colors.white,
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10
     },
     editBtn: {
         left: 10,
@@ -111,6 +105,7 @@ const styles = StyleSheet.create({
         right: 10,
     },
     saveBtn: {
-        fontSize: 50
+        fontSize: 30,
+        color: colors.green,
     }
 })
