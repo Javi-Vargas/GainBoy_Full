dc28f576805d9f333f439a4f77b2fc75299a2cf3
Javi-Vargas <105886463+Javi-Vargas@users.noreply.github.com>
2022-07-27 18:21:51 -0400

    Update WorkoutScreen.js

--- MobileGainBoy/MobileGainBoy/src/screens/WorkoutScreen.js
+++ MobileGainBoy/MobileGainBoy/src/screens/WorkoutScreen.js
@@ -2,57 +2,48 @@ import React from "react";
 import { View, Text, Button, StyleSheet, ScrollView, SafeAreaView, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import Feather from 'react-native-vector-icons/Feather'
-
+import colors from '../assets/colors'
 // PREPROCESSORS for Unit testing
-const UNIT_EDIT_WORKOUT     = false;
+const UNIT_EDIT_WORKOUT = false;
 const UNIT_DISPLAY_WORKOUTS = false;
-const UNIT_DELETE_WORKOUT   = false;
+const UNIT_DELETE_WORKOUT = false;
 
 const WorkoutScreen = ({ navigation }) => {
-    
+
     const logout = () => {
-        global.userId   = "";
-        global.token    = "";
-        global.email    = "";
+        global.email = "";
         global.password = "";
-        global.fullName = "";
-        global.exerciseMap.clear();
+        global.userId = "";
+        global.token = "";
         navigation.navigate('Login');
     }
 
-    const updateWorkout = async () => {
+    const editWorkout = async () => {
         var obj;
 
         // <----------UNIT TESTING----------> 
         //  Automated test for updating a workout
-        if (UNIT_EDIT_WORKOUT)
-        {
-            obj = {_id: global.exerciseMap.get('something'), token: global.token, 
-                   name: 'somethingTheSequel', userId: '777', reps: '99', sets: '99', 
-                   totalWeight: '9000', timeSpent: '99'};
+        if (UNIT_EDIT_WORKOUT) {
+            obj = { name: 'somethingTheSequelAgain', userId: '777', reps: '99', sets: '99', totalWeight: '9000', timeSpent: '99' };
         }
         // --------------------------------->
-        else
-        {
-            //TODO: Need to first check the workout name is a key in the map, then fill accordingly
-            //obj = {_id: , token: global.token, name: , userId: global.userId, reps: , sets: , totalWeight: , timeSpent: };
+        else {
+            obj = { name: 'something', userId: global.userId, reps: '99', sets: '99', totalWeight: '9000', timeSpent: '99' };
         }
-        
+
         var js = JSON.stringify(obj);
 
         const response = await fetch(
             'https://gainzboy.herokuapp.com/auth/updateWorkout',
-            {method:'POST', body:js, headers:{'Content-Type': 'application/json'}}
+            { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } }
         );
 
         var res = JSON.parse(await response.text());
-        
-        if(res.message == undefined)
-        {
+
+        if (res.message == undefined) {
             alert('Error occurred updating workout');
         }
-        else
-        {
+        else {
             alert(res.message);
             // Navigation is a property given from the Stack.Screen component in App.js. Inside this 'navigation' property 
             // is a function called navigate() that takes the name of another screen, in this case 'Landing', again defined in App.js
@@ -65,33 +56,28 @@ const WorkoutScreen = ({ navigation }) => {
 
         // <----------UNIT TESTING----------> 
         //  Automated test for displaying workouts
-        if (UNIT_DISPLAY_WORKOUTS)
-        {
-            obj = {token: global.token, userId: '777'};
+        if (UNIT_DISPLAY_WORKOUTS) {
+            obj = { userId: '777' };
         }
         // --------------------------------->
-        else
-        {
-            obj = {token: global.token, userId: global.userId};
+        else {
+            obj = { userId: global.userId };
         }
-        
+
         var js = JSON.stringify(obj);
 
         const response = await fetch(
             'https://gainzboy.herokuapp.com/auth/displayWorkouts',
-            {method:'POST', body:js, headers:{'Content-Type': 'application/json'}}
+            { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } }
         );
 
         var res = JSON.parse(await response.text());
-        
-        if(res.status != undefined)
-        {
+
+        if (res.status != undefined) {
             alert('Could not display your workouts');
         }
-        else
-        {
-            for (let i = 0; i < res.results.length; i++)
-            {
+        else {
+            for (let i = 0; i < res.results.length; i++) {
                 alert(res.results[i].name);
             }
         }
@@ -102,78 +88,70 @@ const WorkoutScreen = ({ navigation }) => {
 
         // <----------UNIT TESTING----------> 
         //  Automated test for deleting a workout
-        if (UNIT_DELETE_WORKOUT)
-        {
-            obj = {token: global.token, _id: global.exerciseMap.get('something')};
+        if (UNIT_DELETE_WORKOUT) {
+            obj = { name: 'something', userId: '777', reps: '99', sets: '99', totalWeight: '9000', timeSpent: '99' };
         }
         // --------------------------------->
-        else
-        {
-            //TODO: Need to first check the workout name is a key in the map, then fill accordingly
-            //obj = {token: global.token, _id: };
+        else {
+            obj = { name: 'something', userId: global.userId, reps: '99', sets: '99', totalWeight: '9000', timeSpent: '99' };
         }
-        
+
         var js = JSON.stringify(obj);
 
         const response = await fetch(
             'https://gainzboy.herokuapp.com/auth/deleteWorkout',
-            {method:'POST', body:js, headers:{'Content-Type': 'application/json'}}
+            { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } }
         );
 
         var res = JSON.parse(await response.text());
-        
-        if(res.message == undefined)
-        {
+
+        if (res.message == undefined) {
             alert('Could not delete that workout');
         }
-        else
-        {
+        else {
             alert(res.message);
             navigation.navigate('Landing');
         }
     }
-    
+
     return (
         <SafeAreaView style={styles.container}>
-            <ScrollView style={{ paddingTop: 20, paddingHorizontal: 10 }}>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <TouchableOpacity onPress={() => navigation.openDrawer()}>
-                        <Ionicons name="menu-outline" color="blue" size={30} />
-                    </TouchableOpacity>
-                    <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Hi FName</Text>
-                </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: 40, paddingBottom: 20 }}>
-                    <Button title="Edit" onPress={() => {updateWorkout();}} />
-                    <TouchableOpacity onPress={() => navigation.navigate('AddWorkout')}>
-                        <Ionicons name="add-outline" color="blue" size={30} />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ flexDirection: 'row', borderColor: 'black', borderWidth: 1, borderRadius: 8, paddingHorizontal: 10, paddingVertical: 8 }}>
-                    <Feather name='search' size={20} color="black" style={{ marginRight: 5 }} />
-                    <TextInput placeholder="Search Saved Workouts" />
-                </View>
-                <View >
-                    <Text style={{ paddingTop: 60, borderRadius: 20 }}>I wanna make this a list with the users saved Workouts</Text>
-                </View>
-
-                {/*TEMPORARY, delete me or redo me*/}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: 20, paddingHorizontal: 10 }}>
+                <Text style={{ fontSize: 30, fontWeight: 'bold', color: colors.CJpurple }}>Hi User</Text>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: 40, paddingBottom: 20 }}>
+                {/* <Button title="Edit" onPress={() => { editWorkout(); }} /> */}
+                <TouchableOpacity style={{ flexDirection: 'row' }} onPress={() => navigation.navigate('AddWorkout')}>
+                    <Ionicons name="add-outline" color={colors.green} size={30} />
+                    <Text style={{ color: colors.green, paddingTop: 8 }}>Add Workout</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={{ flexDirection: 'row', borderColor: 'black', borderWidth: 1, borderRadius: 8, paddingHorizontal: 10, paddingVertical: 8, backgroundColor: colors.white, margin: 10 }}>
+                <Feather name='search' size={20} color="black" style={{ marginRight: 5 }} />
+                <TextInput placeholder="Search Saved Workouts" />
+            </View>
+            <ScrollView>
                 <View>
-                    <View style={{height: 40}}/>
+                    {/* <View style={{ height: 40 }} /> */}
 
-                    <TouchableOpacity onPress={() => {displayWorkouts();}}>
-                        <Text>Tell me your displaying without showing me it (Click me when unit testing!)</Text>
+                    <TouchableOpacity onPress={() => { displayWorkouts(); }}>
+                        <Text style={{ color: colors.white }}>Tell me your displaying without showing me it (Click me when unit testing!)</Text>
                     </TouchableOpacity>
 
-                    <View style={{height: 40}}/>
+                    <View style={{ height: 40 }} />
 
-                    <TouchableOpacity onPress={() => {deleteWorkout();}}>
-                        <Text>Delete something behind your back (Click me when unit testing!)</Text>
+                    <TouchableOpacity onPress={() => { deleteWorkout(); }}>
+                        <Text style={{ color: colors.white }}>Delete something behind your back (Click me when unit testing!)</Text>
                     </TouchableOpacity>
-
-                    <View style={{height: 40}}/>
-
-                    <Button title="Logout" onPress={() => {logout();}} />
                 </View>
+                <View style={{ justifyContent: 'space-evenly' }}>
+                    {data.map(item => (
+                        <Card data={item} />
+                    ))}
+                </View>
+                {/*TEMPORARY, delete me or redo me*/}
+            </ScrollView>
+            <ScrollView style={{ paddingTop: 20, paddingHorizontal: 10 }}>
             </ScrollView>
         </SafeAreaView >
     )
@@ -181,10 +159,37 @@ const WorkoutScreen = ({ navigation }) => {
 
 export default WorkoutScreen;
 
+
+//Data and Card are used to display the workouts/exercises and all its data. All hardcoded data as of right now however. 
+//I tried but I couldn't get the displayed info in the card to be align to the middle like in the prototype. 
+const data = [
+    { name: 'Arms', reps: 20, sets: 3, totalWeight: 500, timeSpent: 30, lightColor: colors.white },
+    { name: 'Delts', reps: 5, sets: 5, totalWeight: 1500, timeSpent: 60, lightColor: colors.white },
+    { name: 'Legs', reps: 10, sets: 4, totalWeight: 2000, timeSpent: 120, lightColor: colors.white },
+];
+
+const Card = ({ data }) => {
+    return (
+        <View style={{ shadowColor: 'black', shadowRadius: 2, shadowOffset: { width: -5, height: 10, }, shadowOpacity: 0.5, flex: 1, padding: 25, paddingBottom: -20, backgroundColor: data.lightColor, margin: 20, borderRadius: 10 }}>
+            <View style={{ flexDirection: 'column', justifyContent: 'center' }}>
+                <Text>Workout: {data.name} </Text>
+                <Text>Reps: {data.reps}</Text>
+                <Text>Sets: {data.sets} </Text>
+                <Text>Total Weight: {data.totalWeight}</Text>
+                <Text>Time Spent: {data.timeSpent}</Text>
+            </View>
+            <View style={{ paddingTop: 10, flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                <Feather name='edit' size={25} color={colors.CJpurple} style={{ marginRight: 5 }} />
+                <Ionicons name="trash-outline" color={colors.red} size={25} />
+            </View>
+        </View>
+    )
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#8fcbbc",
+        backgroundColor: colors.black,
     },
     addWorkout: {
         flexDirection: 'row',
